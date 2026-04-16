ca62a6ea0ea05ec02a317e415e1101171d06fa3d
Baqtiyar Utepbergenov <utepbergenovbakhtiar04@gmail.com>
2026-04-16 18:06:32 +0500

    API ауысты

--- api.js
+++ api.js
@@ -1,4 +1,8 @@
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+    ? "http://localhost:5000"
+    : "https://digital-literacy-zs48.onrender.com";
 
 function showMessage(message, type = "error") {
   const existingMessage = document.querySelector(".api-message");
