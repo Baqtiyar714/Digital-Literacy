848be3cc59964e8633417e6742f9543e2fec5dd3
Baqtiyar Utepbergenov <utepbergenovbakhtiar04@gmail.com>
2026-04-18 11:27:14 +0500

    Админ өзгеріс

--- admin.js
+++ admin.js
@@ -1,4 +1,8 @@
-const API = "http://localhost:5000";
+const API =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+    ? "http://localhost:5000"
+    : "https://digital-literacy-zs48.onrender.com";
 let adminPassword = "";
 let adminUsername = "";
 
