f38bea12960f1e80537d2ddd4c502a73157597b6
Baqtiyar Utepbergenov <utepbergenovbakhtiar04@gmail.com>
2026-05-12 20:17:47 +0500

    fix smtp ipv6 issue

--- index.js
+++ index.js
@@ -60,10 +60,15 @@ const emailCodes = {};
 // Gmail SMTP арқылы email жіберу үшін баптау
 // EMAIL_USER және EMAIL_PASS .env файлынан алынады
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
