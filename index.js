6d05026b68017634376b7c860afdd0201b3390e0
Baqtiyar Utepbergenov <utepbergenovbakhtiar04@gmail.com>
2026-05-13 00:40:04 +0500

    force ipv4 smtp

--- index.js
+++ index.js
@@ -63,11 +63,16 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   tls: {
+    family: 4,
     rejectUnauthorized: false,
   },
 });
